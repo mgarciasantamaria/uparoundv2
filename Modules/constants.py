ad9e68decf49f795c2fe615731176d29b5b9b15f
Mario Garcia <mandres_garcia@hotmail.com>
2023-06-13 12:30:58 -0500

    Se corrige ruta evo8

--- Modules/constants.py
+++ Modules/constants.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
 #_*_ codig: utf8 _*_
-src_path="/mnt/Aspera/CABLEOPERDORES/VODAROUND_VCMBOG"#"//evo8/Aspera/ing/OUT/"
+src_path="/mnt/Aspera/CABLEOPERADORES/VODAROUND_VCMBOG"#"//evo8/Aspera/ing/OUT/"
 bucket='vod-comp-around'
 json_path="./json/summary.json"
